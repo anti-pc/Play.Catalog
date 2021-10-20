28836518ef3b5cc5985f9a4aa0daf92a0fff466a
buket.culfaoglu <buket.culfaoglu@prestijyazilim.com.tr>
2021-10-20 16:57:54 +0300

    some fixing

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -13,13 +13,6 @@ namespace Play.Catalog.Service.Controllers
     [Route("items")]
     public class ItemsController : ControllerBase
     {
-        // private static readonly List<ItemDto> items = new()
-        // {
-        //     new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
-        //     new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
-        //     new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow),
-        // };
-
         private readonly ItemsRepository itemsRepository = new();
 
         [HttpGet]
@@ -41,13 +34,7 @@ namespace Play.Catalog.Service.Controllers
             }
 
             return item.AsDto();
-            // var item = items.Where(item => item.Id == id).SingleOrDefault();
-            // if (item == null)
-            // {
-            //     return NotFound();
-            // }
 
-            // return item;
         }
 
         //POST /items
@@ -63,12 +50,8 @@ namespace Play.Catalog.Service.Controllers
             };
 
             await itemsRepository.CreateAsync(item);
+
             return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
-
-            // var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
-            // items.Add(item);
-
-            // return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
         }
 
         //Put /items/{id}   public ActionResult<ItemDto> Put(Guid id, UpdateItemDto updateItemDto)
@@ -90,24 +73,6 @@ namespace Play.Catalog.Service.Controllers
 
             return NoContent();
 
-            // var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
-
-            // if (existingItem == null)
-            // {
-            //     return NotFound();
-            // }
-
-            // var updatedItem = existingItem with
-            // {
-            //     Name = updateItemDto.Name,
-            //     Description = updateItemDto.Description,
-            //     Price = updateItemDto.Price
-            // };
-
-            // var index = items.FindIndex(existingItem => existingItem.Id == id);
-            // items[index] = updatedItem;
-
-            // return NoContent();
         }
 
         //Delete /items/{id} 
@@ -125,13 +90,6 @@ namespace Play.Catalog.Service.Controllers
 
             return NoContent();
 
-            // var index = items.FindIndex(existingItem => existingItem.Id == id);
-            // if (index < 0)
-            //     return NotFound();
-
-            // items.RemoveAt(index);
-
-            // return NoContent();
         }
     }
 }
